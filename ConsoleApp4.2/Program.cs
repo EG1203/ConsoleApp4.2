aa62e2a4443d5060368641d791c5a913ce0195a2
МАКСИМ МАКСИМ <m12.12.2003@outlook.com>
2021-12-21 13:56:35 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 4, col = 6;
+            int[,] arr = new int[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (i > j)
+                    {
+                        string v1 = "${arr[i, j}=i";
+                        string v = v1;
+                        string value = v;
+                        Console.Write(value);
+                    }
+                    else
+                    { }
+                }
+            }
+        }
+    }
+}
